b430e966264011b5b667722c2ed1b6e11eb027b0
w.meek <w.meek@metrological.com>
2018-08-09 08:15:19 -0700

    [NexusServer] add resolution  list

--- Compositor/lib/NexusServer/NexusServer.cpp
+++ Compositor/lib/NexusServer/NexusServer.cpp
@@ -16,7 +16,24 @@
 
 BDBG_MODULE(NexusServer);
 
+
+
 namespace WPEFramework {
+    ENUM_CONVERSION_BEGIN(Exchange::IComposition::ScreenResolution)
+
+    { Exchange::IComposition::ScreenResolution_Unknown,   _TXT("Unknown")   },
+    { Exchange::IComposition::ScreenResolution_480i,      _TXT("480i")      },
+    { Exchange::IComposition::ScreenResolution_480p,      _TXT("480p")      },
+    { Exchange::IComposition::ScreenResolution_720p,      _TXT("720p")      },
+    { Exchange::IComposition::ScreenResolution_720p50Hz,  _TXT("720p50Hz")  },
+    { Exchange::IComposition::ScreenResolution_1080p24Hz, _TXT("1080p24Hz") },
+    { Exchange::IComposition::ScreenResolution_1080i50Hz, _TXT("1080i50Hz") },
+    { Exchange::IComposition::ScreenResolution_1080p50Hz, _TXT("1080p50Hz") },
+    { Exchange::IComposition::ScreenResolution_1080p60Hz, _TXT("1080p60Hz") },
+    { Exchange::IComposition::ScreenResolution_2160p50Hz, _TXT("2160p50Hz") },
+    { Exchange::IComposition::ScreenResolution_2160p60Hz, _TXT("2160p60Hz") },
+
+    ENUM_CONVERSION_END(Exchange::IComposition::ScreenResolution)
 namespace Broadcom {
     static const std::map<const Exchange::IComposition::ScreenResolution, const NEXUS_VideoFormat > formatLookup = {
         { Exchange::IComposition::ScreenResolution::ScreenResolution_Unknown, NEXUS_VideoFormat_eUnknown },
