1ac85f85d1844d82a1264033777bf70e5b6e8e74
w.meek <w.meek@metrological.com>
2018-08-23 02:21:26 -0700

    [cmake] add FindSystemd

---
+++ cmake/FindSystemd.cmake
@@ -0,0 +1,18 @@
+# - Try to find Systemd
+# Once done this will define
+
+#  SYSTEMD_FOUND - System has libsystemd
+#  SYSTEMD_INCLUDE_DIRS - The libsystemd include directories
+#  SYSTEMD_LIBRARIES - The libraries needed to use libsystemd
+#  SYSTEMD_DEFINITIONS - The libraries needed to use libsystemd
+
+find_package(PkgConfig)
+pkg_check_modules(PC_SYSTEMD QUIET libsystemd)
+
+find_library(SYSTEMD_LIBRARIES NAMES systemd ${PC_SYSTEMD_LIBRARY_DIRS})
+find_path(SYSTEMD_INCLUDE_DIRS systemd/sd-login.h HINTS ${PC_SYSTEMD_INCLUDE_DIRS})
+set(SYSTEMD_DEFINITIONS ${PC_SYSTEMD_CFLAGS_OTHER})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(SYSTEMD DEFAULT_MSG SYSTEMD_INCLUDE_DIRS SYSTEMD_LIBRARIES)
+mark_as_advanced(SYSTEMD_FOUND SYSTEMD_INCLUDE_DIRS SYSTEMD_LIBRARIES SYSTEMD_DEFINITIONS)
