61d0889917f6c0bb61ced51e9859aec5eacc55f0
Pierre Wielders <pierre@wielders.net>
2018-12-25 12:53:28 +0100

    [CMAKE] The last bits of phase, module/default_targets, removed.

--- cmake/module.cmake
+++
@@ -1,7 +0,0 @@
-if (PLUGIN_NAME STREQUAL "")
-    message(FATAL_ERROR "unexpected: 'PLUGIN_NAME' seems not configured in your plugin's CMakeList.txt.")
-endif()
-
-set(MODULE_NAME ${NAMESPACE}${PLUGIN_NAME})
-
-message("Setting up ${MODULE_NAME}")
