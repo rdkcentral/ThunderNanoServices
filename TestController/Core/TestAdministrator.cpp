a142fb17711b9b4982234e01c19e191dfc709a5f
Damian Danylko <d.danylko@metrological.com>
2019-02-22 13:12:54 +0100

    [TestController] Add TestController plugin
    TestController is a plugin, that allows to execute standalone tests.
    Following REST API is exposed:
    // GET /Service/<CALLSIGN>/TestCategories
    // GET /Service/<CALLSIGN>/<TEST_CATEGORY>/Tests
    // GET /Service/<CALLSIGN>/<TEST_CATEGORY>/<TEST_NAME>/Description
    // POST/PUT /Service/<CALLSIGN>/TestCategories/Run
    // POST/PUT /Service/<CALLSIGN>/<TEST_CATEGORY>/Run
    // POST/PUT /Service/<CALLSIGN>/<TEST_CATEGORY>/<TEST_NAME>

---
+++ TestController/Core/TestAdministrator.cpp
@@ -0,0 +1,58 @@
+#include "TestAdministrator.h"
+
+namespace WPEFramework {
+namespace TestCore {
+
+/* static */ TestAdministrator& TestAdministrator::Instance()
+{
+    static TestAdministrator _singleton;
+    return (_singleton);
+}
+
+void TestAdministrator::Announce(Exchange::ITestController::ICategory* category)
+{
+    ASSERT(category != nullptr);
+
+    _adminLock.Lock();
+    auto found = _testsCategories.find(category->Name());
+    ASSERT((found == _testsCategories.end()) && "Category already exists!");
+
+    if (found == _testsCategories.end())
+    {
+        _testsCategories[category->Name()] = category;
+    }
+    _adminLock.Unlock();
+
+}
+
+void TestAdministrator::Revoke(Exchange::ITestController::ICategory* category)
+{
+    ASSERT(category != nullptr);
+
+    _adminLock.Lock();
+    _testsCategories.erase(category->Name());
+    _adminLock.Unlock();
+}
+
+Exchange::ITestController::ICategory::IIterator* TestAdministrator::Categories()
+{
+    _adminLock.Lock();
+    auto iterator = Core::Service<TestCore::CategoryIterator>::Create<Exchange::ITestController::ICategory::IIterator>(_testsCategories);
+    _adminLock.Unlock();
+    return iterator;
+}
+
+Exchange::ITestController::ICategory* TestAdministrator::Category(const string& name)
+{
+    Exchange::ITestController::ICategory* result = nullptr;
+    _adminLock.Lock();
+    auto found = _testsCategories.find(name);
+    if (found != _testsCategories.end())
+    {
+        result = found->second;
+    }
+    _adminLock.Unlock();
+    return result;
+}
+} // namespace TestCore
+} // namespace WPEFramework
---
+++ TestController/TestController.h
@@ -0,0 +1,155 @@
+#pragma once
+
+#include "Module.h"
+
+#include <interfaces/IMemory.h>
+#include <interfaces/ITestController.h>
+
+#include "Core/TestMetadata.h"
+
+namespace WPEFramework {
+namespace Plugin {
+
+class TestController : public PluginHost::IPlugin, public PluginHost::IWeb {
+public:
+    // maximum wait time for process to be spawned
+    static constexpr uint32_t ImplWaitTime = 1000;
+
+private:
+    class Notification : public RPC::IRemoteProcess::INotification {
+    public:
+        Notification() = delete;
+        Notification(const Notification&) = delete;
+
+        explicit Notification(TestController* parent)
+            : _parent(*parent)
+        {
+            ASSERT(parent != nullptr);
+        }
+        virtual ~Notification() {}
+
+    public:
+        virtual void Activated(RPC::IRemoteProcess* process) { _parent.Activated(process); }
+
+        virtual void Deactivated(RPC::IRemoteProcess* process) { _parent.Deactivated(process); }
+
+        BEGIN_INTERFACE_MAP(Notification)
+        INTERFACE_ENTRY(RPC::IRemoteProcess::INotification)
+        END_INTERFACE_MAP
+
+    private:
+        TestController& _parent;
+    };
+
+    class MetadataCategory : public Core::JSON::Container {
+        public:
+            MetadataCategory(const MetadataCategory&) = delete;
+            MetadataCategory& operator=(const MetadataCategory&) = delete;
+
+            MetadataCategory()
+                : Core::JSON::Container()
+                , TestCategories()
+            {
+                Add(_T("testCategories"), &TestCategories);
+            }
+            ~MetadataCategory() {}
+
+        public:
+            Core::JSON::ArrayType<Core::JSON::String> TestCategories;
+    };
+
+    class MetadataTest : public Core::JSON::Container {
+        public:
+            MetadataTest(const MetadataTest&) = delete;
+            MetadataTest& operator=(const MetadataTest&) = delete;
+
+            MetadataTest()
+                : Core::JSON::Container()
+                , Tests()
+            {
+                Add(_T("tests"), &Tests);
+            }
+            ~MetadataTest() {}
+
+        public:
+            Core::JSON::ArrayType<Core::JSON::String> Tests;
+    };
+
+    class OverallTestResults : public Core::JSON::Container {
+        private:
+            OverallTestResults(const OverallTestResults&) = delete;
+            OverallTestResults& operator=(const OverallTestResults&) = delete;
+
+        public:
+            OverallTestResults()
+                : Core::JSON::Container()
+                , Results()
+            {
+                Add(_T("testsResults"), &Results);
+            }
+
+            ~OverallTestResults() = default;
+
+        public:
+            Core::JSON::ArrayType<TestCore::TestResult> Results;
+    };
+
+public:
+    TestController()
+        : _service(nullptr)
+        , _notification(this)
+        , _memory(nullptr)
+        , _testControllerImp(nullptr)
+        , _skipURL(0)
+        , _pid(0)
+        , _prevCategory(EMPTY_STRING)
+    {
+    }
+
+    virtual ~TestController() {}
+
+    BEGIN_INTERFACE_MAP(TestController)
+        INTERFACE_ENTRY(PluginHost::IPlugin)
+        INTERFACE_ENTRY(PluginHost::IWeb)
+        INTERFACE_AGGREGATE(Exchange::IMemory, _memory)
+        INTERFACE_AGGREGATE(Exchange::ITestController, _testControllerImp)
+    END_INTERFACE_MAP
+
+    //   IPlugin methods
+    // -------------------------------------------------------------------------------------------------------
+    virtual const string Initialize(PluginHost::IShell* service) override;
+    virtual void Deinitialize(PluginHost::IShell* service) override;
+    virtual string Information() const override;
+
+    //  IWeb methods
+    // -------------------------------------------------------------------------------------------------------
+    virtual void Inbound(Web::Request& request);
+    virtual Core::ProxyType<Web::Response> Process(const Web::Request& request);
+
+    TestController(const TestController&) = delete;
+    TestController& operator=(const TestController&) = delete;
+private:
+
+    void Activated(RPC::IRemoteProcess* process);
+    void Deactivated(RPC::IRemoteProcess* process);
+
+    void ProcessTermination(uint32_t pid);
+
+    void TestPreparation(Exchange::ITestController::ICategory* const category, const string& categoryName);
+    string /*JSON*/ HandleRequest(Web::Request::type type, const string& path, const uint8_t skipUrl, const string& body /*JSON*/);
+    string /*JSON*/ TestCategories(Exchange::ITestController::ICategory::IIterator* categories);
+    string /*JSON*/ Tests(Exchange::ITestController::ITest::IIterator* tests);
+    string /*JSON*/ RunAll(const string& body, const string& categoryName = EMPTY_STRING);
+    string /*JSON*/ RunTest(const string& body, const string& categoryName, const string& testName);
+
+    PluginHost::IShell* _service;
+    Core::Sink<Notification> _notification;
+    Exchange::IMemory* _memory;
+    Exchange::ITestController* _testControllerImp;
+    uint8_t _skipURL;
+    uint32_t _pid;
+    string _prevCategory;
+};
+
+} // namespace Plugin
+} // namespace WPEFramework
