0f2e9a6cc941e470cd96c97f8a33881e2a0f8b47
HaseenaSainul <h.sainul@metrological.com>
2019-05-10 20:38:47 +0530

    [CMake] GStreamer.cmake updated and added GStreamer Cmake for GST Video

--- cmake/FindGStreamerVideo.cmake
+++ cmake/FindGStreamerVideo.cmake
@@ -1,8 +1,8 @@
-# - Try to find gstreamer-1.0.
+# - Try to find gstreamer-video-1.0.
 # Once done, this will define
 #
-#  GSTREAMER_FOUND - the gstreamer-1.0 is available
-#  GStreamer::GStreamer - The gstreamer-1.0 library and all its dependecies
+#  GSTREAMER_VIDEO_FOUND - the gstreamer-video-1.0 is available
+#  GStreamerVideo::GStreamerVideo - The gstreamer-video-1.0 library and all its dependecies
 #
 # Copyright (C) 2019 Metrological B.V
 #
@@ -28,33 +28,33 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 find_package(PkgConfig)
-pkg_check_modules(PC_GSTREAMER gstreamer-1.0)
+pkg_check_modules(PC_GSTREAMER_VIDEO gstreamer-video-1.0)
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(PC_GSTREAMER DEFAULT_MSG PC_GSTREAMER_FOUND)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(PC_GSTREAMER_VIDEO DEFAULT_MSG PC_GSTREAMER_VIDEO_FOUND)
 
-mark_as_advanced(PC_GSTREAMER_INCLUDE_DIRS PC_GSTREAMER_LIBRARIES PC_GSTREAMER_LIBRARY_DIRS)
+mark_as_advanced(PC_GSTREAMER_VIDEO_INCLUDE_DIRS PC_GSTREAMER_VIDEO_LIBRARIES PC_GSTREAMER_VIDEO_LIBRARY_DIRS)
 
-if(${PC_GSTREAMER_FOUND})
-    find_library(GSTREAMER_LIBRARY gstreamer-1.0
-        HINTS ${PC_GSTREAMER_LIBRARY_DIRS}
+if(${PC_GSTREAMER_VIDEO_FOUND})
+    find_library(GSTREAMER_VIDEO_LIBRARY gstvideo-1.0
+        HINTS ${PC_GSTREAMER_VIDEO_LIBRARY_DIRS}
     )
 
-    set(GSTREAMER_LIBRARIES ${PC_GSTREAMER_LIBRARIES})
-    set(GSTREAMER_INCLUDES ${PC_GSTREAMER_INCLUDE_DIRS})
-    set(GSTREAMER_FOUND ${PC_GSTREAMER_FOUND})
+    set(GSTREAMER_VIDEO_LIBRARIES ${PC_GSTREAMER_VIDEO_LIBRARIES})
+    set(GSTREAMER_VIDEO_INCLUDES ${PC_GSTREAMER_VIDEO_INCLUDE_DIRS})
+    set(GSTREAMER_VIDEO_FOUND ${PC_GSTREAMER_VIDEO_FOUND})
 
-    if(NOT TARGET GStreamer::GStreamer)
-        add_library(GStreamer::GStreamer UNKNOWN IMPORTED)
+    if(NOT TARGET GStreamerVideo::GStreamerVideo)
+        add_library(GStreamerVideo::GStreamerVideo UNKNOWN IMPORTED)
 
-        set_target_properties(GStreamer::GStreamer
+        set_target_properties(GStreamerVideo::GStreamerVideo
                 PROPERTIES
                 IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-                IMPORTED_LOCATION "${GSTREAMER_LIBRARY}"
-                INTERFACE_COMPILE_DEFINITIONS "GSTREAMER"
-                INTERFACE_COMPILE_OPTIONS "${PC_GSTREAMER_CFLAGS_OTHER}"
-                INTERFACE_INCLUDE_DIRECTORIES "${PC_GSTREAMER_INCLUDE_DIRS}"
-                INTERFACE_LINK_LIBRARIES "${PC_GSTREAMER_LIBRARIES}"
+                IMPORTED_LOCATION "${GSTREAMER_VIDEO_LIBRARY}"
+                INTERFACE_COMPILE_DEFINITIONS "GSTREAMER_VIDEO"
+                INTERFACE_COMPILE_OPTIONS "${PC_GSTREAMER_VIDEO_CFLAGS_OTHER}"
+                INTERFACE_INCLUDE_DIRECTORIES "${PC_GSTREAMER_VIDEO_INCLUDE_DIRS}"
+                INTERFACE_LINK_LIBRARIES "${PC_GSTREAMER_VIDEO_LIBRARIES}"
                 )
     endif()
 endif()
