a160b286eca766f10bbeee5170987a16fe55ee4b
sebaszm <s.szczepaniak@metrological.com>
2019-07-12 12:59:40 +0200

    [Streamer] Move BitArray to framework core

--- Streamer/Administrator.h
+++ Streamer/Administrator.h
@@ -48,7 +48,7 @@ namespace Player {
         private:
             Core::CriticalSection _adminLock;
             std::map<string, IPlayerPlatformFactory*> _streamers;
-            BitArrayType<16> _slots;
+            Core::BitArrayType<16> _slots;
         };
 
         template<class PLAYER>
