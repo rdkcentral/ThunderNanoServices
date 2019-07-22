fd70e9a99e14844577d8195f8c053838129c3dfb
Pierre Wielders <pierre@wielders.net>
2019-07-22 10:29:21 +0200

    [PACKAGER] Prepare for open sourcing.

---
+++ Packager/Module.h
@@ -0,0 +1,14 @@
+#ifndef __MODULE_PLUGIN_PACKAGER_H
+#define __MODULE_PLUGIN_PACKAGER_H
+
+#ifndef MODULE_NAME
+#define MODULE_NAME Plugin_Packager
+#endif
+
+#include <tracing/tracing.h>
+#include <plugins/plugins.h>
+
+#undef EXTERNAL
+#define EXTERNAL
+
+#endif // __MODULE_PLUGIN_PACKAGER_H
