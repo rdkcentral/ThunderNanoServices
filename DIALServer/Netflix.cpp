590c080764c0e96e4f000776cb6bb50a597c205c
sebaszm <s.szczepaniak@metrological.com>
2020-07-21 14:18:22 +0200

    [DIALServer] Allow passing payload in passive mode

--- DIALServer/Netflix.cpp
+++ DIALServer/Netflix.cpp
@@ -25,44 +25,9 @@
 namespace WPEFramework {
 namespace DIALHandlers {
 
-    class Netflix : public Plugin::DIALServer::Default {
-    public:
-        Netflix() = delete;
-        Netflix(const Netflix&) = delete;
-        Netflix& operator=(const Netflix&) = delete;
-
-        #ifdef __WINDOWS__
-        #pragma warning(disable : 4355)
-        #endif
-        Netflix(PluginHost::IShell* service, const Plugin::DIALServer::Config::App& config, Plugin::DIALServer *parent)
-            : Default(service, config, parent)
-            , _netflix(nullptr)
-            , _service(nullptr)
-            , _notification(*this)
-            , _hidden(false)
-            , _lock()
-            , _callsign(config.Callsign.Value())
-        {
-            ASSERT(service != nullptr);
-            ASSERT(parent != nullptr);
-            if (_callsign.empty() == false) {
-                service->Register(&_notification);
-            }
-        }
-        #ifdef __WINDOWS__
-        #pragma warning(default : 4355)
-        #endif
-
-        ~Netflix() override
-        {
-            Detach();
-            if (_callsign.empty() == false) {
-                _service->Unregister(&_notification);
-            }
-        }
-
-    public:
-        uint32_t Start(const string& params, const string& payload) override
+    struct Netflix {
+    private:
+        static string Query(const string& params, const string& payload)
         {
             string query = params;
 
@@ -76,129 +41,186 @@ namespace DIALHandlers {
                 query = query + _T("&dial=") + encodedPayload;
             }
 
-            if (_callsign.empty() == false) {
+            return (query);
+        }
+
+    public:
+        class Passive : public Plugin::DIALServer::Default {
+        public:
+            Passive() = delete;
+            Passive(const Passive&) = delete;
+            Passive& operator=(const Passive&) = delete;
+
+        public:
+            Passive(PluginHost::IShell* service, const Plugin::DIALServer::Config::App& config, Plugin::DIALServer *parent)
+                : Default(service, config, parent)
+            {
+            }
+            ~Passive() = default;
+
+        public:
+            uint32_t Start(const string& params, const string& payload) override
+            {
+                return (Default::Start(Query(params, payload), {}));
+            }
+        }; // class Passive
+
+        class Active : public Plugin::DIALServer::Default {
+        public:
+            Active() = delete;
+            Active(const Active&) = delete;
+            Active& operator=(const Active&) = delete;
+
+        public:
+            Active(PluginHost::IShell* service, const Plugin::DIALServer::Config::App& config, Plugin::DIALServer *parent)
+                : Default(service, config, parent)
+                , _netflix(nullptr)
+                , _service(nullptr)
+                , _notification(*this)
+                , _hidden(false)
+                , _lock()
+                , _callsign(config.Callsign.Value())
+            {
+                ASSERT(service != nullptr);
+                ASSERT(parent != nullptr);
+                service->Register(&_notification);
+            }
+            ~Active() override
+            {
+                Detach();
+                _service->Unregister(&_notification);
+            }
+
+        public:
+            uint32_t Start(const string& params, const string& payload) override
+            {
+                const string query = Query(params, payload);
+
                 // Set custom query paramters
                 Core::SystemInfo::SetEnvironment(_T("ONE_TIME_QUERY_STRING_OVERRIDE"), query.c_str());
-            }
 
-            return (Default::Start(query, {}));
-        }
-        void Stop(const string& params, const string& payload) override
-        {
-            Detach();
-            Default::Stop(params, payload);
-        }
-        bool Connect() override
-        {
-            Attach();
-            return (_netflix != nullptr);
-        }
-        bool IsConnected() override
-        {
-            return (_netflix != nullptr);
-        }
-        bool HasHideAndShow() const override
-        {
-            return (_netflix != nullptr);
-        }
-        uint32_t Show() override
-        {
-            _lock.Lock();
-            uint32_t result = Core::ERROR_NONE;
-            _hidden = false;
-            if (_netflix != nullptr) {
-                _netflix->SetVisible(true);
-            } else {
-                result = Core::ERROR_GENERAL;
+                return (Default::Start(query, {}));
             }
-            _lock.Unlock();
-            return (result);
-        }
-        void Hide() override
-        {
-            _lock.Lock();
-            if (_netflix != nullptr) {
-                _netflix->SetVisible(false);
-                // TODO: Poor man's state tracking; would be optimal to fetch the state from Netflix itself.
-                _hidden = true;
-            }
-            _lock.Unlock();
-        }
-        bool IsHidden() const override
-        {
-            return (_hidden);
-        }
-        bool URL(const string& /* url */, const string& /* payload */) override
-        {
-            return (false);
-        }
-
-    private:
-        const string& Callsign() const
-        {
-            return (_callsign);
-        }
-        void Attach()
-        {
-            _lock.Lock();
-            if (_netflix == nullptr) {
-                _netflix = Plugin::DIALServer::Default::QueryInterface<Exchange::INetflix>();
-            }
-            _lock.Unlock();
-        }
-        void Detach()
-        {
-            _lock.Lock();
-            _hidden = false;
-            if (_netflix != nullptr) {
-                _netflix->Release();
-                _netflix = nullptr;
-            }
-            _lock.Unlock();
-        }
-
-    private:
-        class Notification : public PluginHost::IPlugin::INotification {
-        public:
-            Notification() = delete;
-            Notification(const Notification&) = delete;
-            Notification& operator=(const Notification&) = delete;
-            Notification(Netflix& parent)
-                : _parent(parent)
+            void Stop(const string& params, const string& payload) override
             {
+                Detach();
+                Default::Stop(params, payload);
             }
-            ~Notification() = default;
-
-        public:
-            void StateChange(PluginHost::IShell* shell) override
+            bool Connect() override
             {
-                ASSERT(shell != nullptr);
-                if (shell->Callsign() == _parent.Callsign()) {
-                    if (shell->State() == PluginHost::IShell::ACTIVATED) {
-                        _parent.Attach();
-                    } else if (shell->State() == PluginHost::IShell::DEACTIVATED) {
-                        _parent.Detach();
-                    }
+                Attach();
+                return (_netflix != nullptr);
+            }
+            bool IsConnected() override
+            {
+                return (_netflix != nullptr);
+            }
+            bool HasHideAndShow() const override
+            {
+                return (_netflix != nullptr);
+            }
+            uint32_t Show() override
+            {
+                _lock.Lock();
+                uint32_t result = Core::ERROR_NONE;
+                _hidden = false;
+                if (_netflix != nullptr) {
+                    _netflix->SetVisible(true);
+                } else {
+                    result = Core::ERROR_GENERAL;
                 }
+                _lock.Unlock();
+                return (result);
             }
-
-        public:
-            BEGIN_INTERFACE_MAP(Notification)
-                INTERFACE_ENTRY(PluginHost::IPlugin::INotification)
-            END_INTERFACE_MAP
+            void Hide() override
+            {
+                _lock.Lock();
+                if (_netflix != nullptr) {
+                    _netflix->SetVisible(false);
+                    // TODO: Poor man's state tracking; would be optimal to fetch the state from Netflix itself.
+                    _hidden = true;
+                }
+                _lock.Unlock();
+            }
+            bool IsHidden() const override
+            {
+                return (_hidden);
+            }
+            bool URL(const string& /* url */, const string& /* payload */) override
+            {
+                return (false);
+            }
 
         private:
-            Netflix& _parent;
-        }; // class Notification
+            const string& Callsign() const
+            {
+                return (_callsign);
+            }
+            void Attach()
+            {
+                _lock.Lock();
+                if (_netflix == nullptr) {
+                    _netflix = Plugin::DIALServer::Default::QueryInterface<Exchange::INetflix>();
+                }
+                _lock.Unlock();
+            }
+            void Detach()
+            {
+                _lock.Lock();
+                _hidden = false;
+                if (_netflix != nullptr) {
+                    _netflix->Release();
+                    _netflix = nullptr;
+                }
+                _lock.Unlock();
+            }
 
-    private:
-        Exchange::INetflix* _netflix;
-        PluginHost::IShell* _service;
-        Core::Sink<Notification> _notification;
-        bool _hidden;
-        mutable Core::CriticalSection _lock;
-        string _callsign;
-    };
+        private:
+            class Notification : public PluginHost::IPlugin::INotification {
+            public:
+                Notification() = delete;
+                Notification(const Notification&) = delete;
+                Notification& operator=(const Notification&) = delete;
+
+            public:
+                explicit Notification(Active& parent)
+                    : _parent(parent)
+                {
+                }
+                ~Notification() = default;
+
+            public:
+                void StateChange(PluginHost::IShell* shell) override
+                {
+                    ASSERT(shell != nullptr);
+                    if (shell->Callsign() == _parent.Callsign()) {
+                        if (shell->State() == PluginHost::IShell::ACTIVATED) {
+                            _parent.Attach();
+                        } else if (shell->State() == PluginHost::IShell::DEACTIVATED) {
+                            _parent.Detach();
+                        }
+                    }
+                }
+
+            public:
+                BEGIN_INTERFACE_MAP(Notification)
+                    INTERFACE_ENTRY(PluginHost::IPlugin::INotification)
+                END_INTERFACE_MAP
+
+            private:
+                Active& _parent;
+            }; // class Notification
+
+        private:
+            Exchange::INetflix* _netflix;
+            PluginHost::IShell* _service;
+            Core::Sink<Notification> _notification;
+            bool _hidden;
+            mutable Core::CriticalSection _lock;
+            string _callsign;
+        }; // class Active
+
+    }; // class Netflix
 
     static Plugin::DIALServer::ApplicationRegistrationType<Netflix> _netflixHandler;
 }
