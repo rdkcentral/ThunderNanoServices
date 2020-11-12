e737250a72c3a09da961263902f5c4cd2818aafb
Neeraj Deshpande <n.deshpande@metrological.com>
2020-11-12 18:31:12 +0530

    Modified to pass debug IP to Cobalt as command line parameter.

--- Cobalt/CobaltImplementation.cpp
+++ Cobalt/CobaltImplementation.cpp
@@ -158,6 +158,7 @@ private:
         CobaltWindow()
             : Core::Thread(0, _T("Cobalt"))
             , _url{"https://www.youtube.com/tv"}
+            , _debugListenIp("0.0.0.0")
             , _debugPort()
         {
         }
@@ -257,6 +258,7 @@ private:
                 string url(config.Inspector.Value());
                 auto pos = url.find(":");
                 if (pos != std::string::npos) {
+                    _debugListenIp = url.substr(0, pos);
                     _debugPort = static_cast<uint16_t>(std::atoi(url.substr(pos + 1).c_str()));
                 }
             }
@@ -292,15 +294,17 @@ private:
         uint32_t Worker() override
         {
             const std::string cmdURL = "--url=" + _url;
+            const std::string cmdDebugListenIp = "--dev_servers_listen_ip=" + _debugListenIp;
             const std::string cmdDebugPort = "--remote_debugging_port=" + std::to_string(_debugPort);
-            const char* argv[] = {"Cobalt", cmdURL.c_str(), cmdDebugPort.c_str()};
+            const char* argv[] = {"Cobalt", cmdURL.c_str(), cmdDebugListenIp.c_str(), cmdDebugPort.c_str()};
             while (IsRunning() == true) {
-                StarboardMain(3, const_cast<char**>(argv));
+                StarboardMain(4, const_cast<char**>(argv));
             }
             return (Core::infinite);
         }
 
         string _url;
+        string _debugListenIp;
         uint16_t _debugPort;
     };
 
