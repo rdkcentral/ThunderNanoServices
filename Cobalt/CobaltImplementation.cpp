4e055912cd1cd7d1226d27ce07776076a8ebad36
HaseenaSainul <h.sainul@metrological.com>
2021-02-01 20:21:13 +0530

    Cobalt: Identifier implementation updated to get the Identifer value

--- Cobalt/CobaltImplementation.cpp
+++ Cobalt/CobaltImplementation.cpp
@@ -434,21 +434,33 @@ public:
     }
 
     uint32_t Identifier(string& id) const override {
-        const PluginHost::ISubSystem::IIdentifier* identifier(_service->SubSystems()->Get<PluginHost::ISubSystem::IIdentifier>());
 
-        if (identifier != nullptr) {
-            uint8_t buffer[64];
+        PluginHost::ISubSystem* subSystems(_service->SubSystems());
+        if (subSystems != nullptr) {
 
-            buffer[0] = static_cast<const PluginHost::ISubSystem::IIdentifier*>(identifier)
-                            ->Identifier(sizeof(buffer) - 1, &(buffer[1]));
+            const Core::IUnknown* base(_service->SubSystems()->Get<PluginHost::ISubSystem::IIdentifier>());
+            if (base != nullptr) {
+                const PluginHost::ISubSystem::IIdentifier* identifier = base->QueryInterface<const PluginHost::ISubSystem::IIdentifier>();
 
-            if (buffer[0] != 0) {
-                id = Core::SystemInfo::Instance().Id(buffer, ~0);
+                if (identifier != nullptr) {
+                    uint8_t buffer[64];
+
+                    buffer[0] = static_cast<const PluginHost::ISubSystem::IIdentifier*>(identifier)
+                                ->Identifier(sizeof(buffer) - 1, &(buffer[1]));
+
+                    if (buffer[0] != 0) {
+                        id = Core::SystemInfo::Instance().Id(buffer, ~0);
+                    }
+
+                    identifier->Release();
+                }
+
+                base->Release();
             }
 
-            identifier->Release();
+            subSystems->Release();
         }
-        return Core::ERROR_NOT_SUPPORTED;
+        return Core::ERROR_NONE;
     }
 
     uint32_t ContentLink(const string& link) override {
