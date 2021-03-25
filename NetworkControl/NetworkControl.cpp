a6771de37bbc8527d3e43769eb7839ddbd4c0044
Bram Oosterhuis <mail@bybram.com>
2021-03-25 16:41:38 +0100

    use new Core::File constructor

--- NetworkControl/NetworkControl.cpp
+++ NetworkControl/NetworkControl.cpp
@@ -96,7 +96,7 @@ namespace Plugin
         _dnsFile = config.DNSFile.Value();
 
         // We will only "open" the DNS resolve file, so of ot does not exist yet, create an empty file.
-        Core::File dnsFile(_dnsFile, true);
+        Core::File dnsFile(_dnsFile);
         if (dnsFile.Exists() == false) {
             if (dnsFile.Create() == false) {
                 SYSLOG(Logging::Startup, (_T("Could not create DNS configuration file [%s]"), _dnsFile.c_str()));
