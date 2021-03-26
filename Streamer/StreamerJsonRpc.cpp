f278a95055ce4a4ad10d962510911a47d093d6d2
Pierre Wielders <pierre@wielders.net>
2021-03-26 14:10:15 +0100

    [MODULE] With the EarlyWarning enabled it becomes even more important to ALWAYS first include the Module.h

--- Streamer/StreamerJsonRpc.cpp
+++ Streamer/StreamerJsonRpc.cpp
@@ -17,9 +17,9 @@
  * limitations under the License.
  */
  
+#include "Module.h"
 #include <interfaces/json/JsonData_Streamer.h>
 #include "Streamer.h"
-#include "Module.h"
 
 namespace WPEFramework {
 
