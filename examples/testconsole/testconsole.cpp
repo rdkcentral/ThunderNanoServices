e6de473176a5b5d54545828b8c86289626d04a8f
Vinod Sathyaseelan <vinod_sathyaseelan@comcast.com>
2021-08-31 13:51:02 -0700

    Added Copyright description

--- examples/testconsole/testconsole.cpp
+++ examples/testconsole/testconsole.cpp
@@ -1,3 +1,22 @@
+ /*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2020 Metrological
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // WebConsole.cpp : Defines the entry point for the console application.
 //
 
@@ -369,8 +388,7 @@ int main(int argc, char** argv)
     ConsoleOptions options(argc, argv);
 
     fprintf(stderr, "TestConsole\n"
-                    "(C) Copyright 2013 Pierre Wielders <pierre@wielders.net>\n"
-                    "licensed under LGPL2.1\n");
+                    "By Pierre Wielders <pierre@wielders.net>\n");
 
     if ((argc < 2) || (options.RequestUsage())) {
         fprintf(stderr, "Usage: TestConsole "
--- examples/testserver/testserver.cpp
+++ examples/testserver/testserver.cpp
@@ -1,3 +1,22 @@
+ /*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2020 Metrological
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "EchoProtocol.h"
 
 namespace WPEFramework {
@@ -73,7 +92,7 @@ namespace TestSystem {
         ConsoleOptions options(argc, argv);
 
         fprintf(stderr, "TestServer\n"
-                        "(C) Copyright 2014 Pierre Wielders <pierre@wielders.net>\n");
+                        "By Pierre Wielders <pierre@wielders.net>\n");
 
         if (options.RequestUsage()) {
             fprintf(stderr, "Usage: TestServer "
