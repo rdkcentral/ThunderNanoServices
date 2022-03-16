186fe09371f675ee82d3772cae69c557ae0a05ef
Haseena Sainul <h.sainul@metrological.com>
2022-03-16 07:17:22 -0700

    Warning: fixed some Wunused-parameter warnings

--- AVS/Impl/SmartScreen/SmartScreen.cpp
+++ AVS/Impl/SmartScreen/SmartScreen.cpp
@@ -164,7 +164,7 @@ namespace Plugin {
         return status;
     }
 
-    bool SmartScreen::Init(const std::string& audiosource, const bool enableKWD, const std::string& pathToInputFolder)
+    bool SmartScreen::Init(const std::string& audiosource, const bool enableKWD, const std::string& pathToInputFolder VARIABLE_IS_NOT_USED)
     {
         auto config = avsCommon::utils::configuration::ConfigurationNode::getRoot();
 
