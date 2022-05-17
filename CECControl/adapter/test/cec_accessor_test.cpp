33947547087e4cf04ad44aca4c638454000c3335
Bram Oosterhuis <mail@bybram.com>
2022-05-17 19:50:49 +0200

    CECControl: update accessorr test

--- CECControl/adapter/test/cec_accessor_test.cpp
+++ CECControl/adapter/test/cec_accessor_test.cpp
@@ -31,12 +31,14 @@
 #include <CECAdapter.h>
 #include <CECIAccessor.h>
 #include <CECProcessor.h>
+#include <CECTypes.h>
 #include <Messages.h>
 
 #include <core/core.h>
 #include <localtracer/localtracer.h>
-// #include <messaging/messaging.h>
 #include <tracing/tracing.h>
+
+#include "../CECAccessor.h"
 
 MODULE_NAME_DECLARATION(BUILD_REFERENCE)
 
@@ -101,6 +103,26 @@ public:
         Core::JSON::String Config;
     };
 
+    class InterfaceConfig : public Core::JSON::Container {
+    public:
+        InterfaceConfig(const InterfaceConfig&) = delete;
+        InterfaceConfig& operator=(const InterfaceConfig&) = delete;
+
+        InterfaceConfig()
+            : Node()
+            , Roles()
+        {
+            Add(_T("node"), &Node);
+            Add(_T("roles"), &Roles);
+        }
+
+        ~InterfaceConfig() = default;
+
+    public:
+        Core::JSON::String Node;
+        Core::JSON::ArrayType<Core::JSON::EnumType<cec_adapter_role_t>> Roles;
+    };
+
 public:
     Config(const Config&) = delete;
     Config& operator=(const Config&) = delete;
@@ -117,24 +139,6 @@ public:
     Core::JSON::ArrayType<Core::JSON::String> Interfaces;
 };
 
-// constexpr char topconfig[] = "{\"interfaces\":[\
-//     {\
-//         \"name\":\"hdmi1\",\
-//         \"configuration\":{\"node\":\"/dev/cec0\",\"roles\":[\"recorder\"]}\
-//     },\
-//     {\
-//         \"name\":\"hdmi2\",\
-//         \"configuration\":{\"node\":\"/dev/cec1\",\"roles\":[\"playback\"]}\
-//     }\
-// ]}";
-
-// constexpr char topconfig[] = "{\"interfaces\":[\
-//     {\
-//         \"name\":\"hdmi1\",\
-//         \"configuration\":{\"node\":\"0\",\"roles\":[\"recorder\"]}\
-//     },\
-// ]}";
-
 class App {
 private:
 public:
@@ -148,7 +152,8 @@ public:
             _config.IElement::FromFile(configFile);
             configFile.Close();
         } else {
-            std::cerr << "Failed to open " << filename << std::endl << std::flush;
+            std::cerr << "Failed to open " << filename << std::endl
+                      << std::flush;
         }
 
         auto index(_config.Interfaces.Elements());
@@ -160,6 +165,30 @@ public:
             string name = iface.Name.Value();
             string config = iface.Config.Value();
             WPEFramework::CEC::IAccessor::Instance()->Announce(name, config);
+
+            Config::InterfaceConfig ifaceconfig;
+            ifaceconfig.FromString(config);
+
+            InterfaceMap::iterator index = _interfaces.find(name);
+
+            if (index == _interfaces.end()) {
+                index = _interfaces.emplace(
+                                       std::piecewise_construct,
+                                       std::forward_as_tuple(name),
+                                       std::forward_as_tuple(device_list_t()))
+                            .first;
+            }
+
+            if (index != _interfaces.end()) {
+                TRACE(Trace::Information, ("Register Roles for %s", (index->first).c_str()));
+
+                auto roles(ifaceconfig.Roles.Elements());
+
+                while (roles.Next() == true) {
+                    TRACE(Trace::Information, ("Register Role 0x%02X ", roles.Current().Value()));
+                    (index->second).push_back(roles.Current().Value());
+                }
+            }
         }
 
         ShowMenu();
@@ -190,8 +219,12 @@ public:
             character = toupper(GetUserInput());
 
             switch (character) {
-            case 'S': {
-                WPEFramework::CEC::Adapter adapter = WPEFramework::CEC::IAccessor::Instance()->GetAdapter("hdmi1", CEC_DEVICE_RECORDER);
+            case 'V': {
+                cec_adapter_role_t role;
+                string interface;
+                GetAdapter(interface, role);
+
+                WPEFramework::CEC::Adapter adapter = WPEFramework::CEC::IAccessor::Instance()->GetAdapter(interface, role);
 
                 if (adapter.IsValid()) {
                     for (uint8_t dest = CEC_LOGICAL_ADDRESS_TV; dest <= CEC_LOGICAL_ADDRESS_SPECIFIC; dest++) {
@@ -215,7 +248,12 @@ public:
                 break;
             }
             case 'P': {
-                WPEFramework::CEC::Adapter adapter = WPEFramework::CEC::IAccessor::Instance()->GetAdapter("hdmi1", CEC_DEVICE_RECORDER);
+                cec_adapter_role_t role;
+                string interface;
+                GetAdapter(interface, role);
+
+                WPEFramework::CEC::Adapter adapter = WPEFramework::CEC::IAccessor::Instance()->GetAdapter(interface, role);
+
                 if (adapter.IsValid()) {
                     for (uint8_t dest = CEC_LOGICAL_ADDRESS_TV; dest <= CEC_LOGICAL_ADDRESS_SPECIFIC; dest++) {
                         Message::Poll poll;
@@ -238,11 +276,108 @@ public:
     }
 
 private:
-    role_t SelectRole()
+    const string ToString(cec_adapter_role_t role)
+    {
+        string result;
+
+        switch (role) {
+        case CEC_DEVICE_TV: {
+            result = "TV";
+            break;
+        }
+        case CEC_DEVICE_RECORDER: {
+            result = "Recorder";
+            break;
+        }
+        case CEC_DEVICE_PLAYBACK: {
+            result = "Playback";
+            break;
+        }
+        case CEC_DEVICE_AUDIOSYSTEM: {
+            result = "AudioSystem";
+            break;
+        }
+        case CEC_DEVICE_SWITCH: {
+            result = "Switch";
+            break;
+        }
+        case CEC_DEVICE_VIDEOPROCESSOR: {
+            result = "VideoProcessor";
+            break;
+        }
+        default:
+            result = "Unknown";
+        }
+
+        return result;
+    }
+
+    void GetAdapter(string& uIface, cec_adapter_role_t& uRole)
+    {
+        if (_interfaces.empty() == false) {
+            int character(CEC_LOGICAL_ADDRESS_UNREGISTERED);
+            uint8_t idx = 1;
+
+            InterfaceMap::const_iterator interfaces = _interfaces.cbegin();
+
+            if (_interfaces.size() == 1) {
+                std::cout << interfaces->first << " was auto selected for the adapter" << std::endl
+                          << std::flush;
+            } else {
+                std::cout << "Choose device:" << std::endl
+                          << std::flush;
+
+                while (interfaces != _interfaces.cend()) {
+                    std::cout << " " << unsigned(idx++) << ". : " << interfaces->first << std::endl;
+                    interfaces++;
+                }
+                uint8_t interface(0);
+
+                character = toupper(GetUserInput());
+                interface = static_cast<uint8_t>(strtol((char*)&character, NULL, 10)) - 1;
+
+                interfaces = _interfaces.cbegin();
+                std::advance(interfaces, interface);
+            }
+
+            uIface = interfaces->first;
+
+            if ((interfaces->second).empty() == false) {
+                device_list_t::const_iterator roles = (interfaces->second).cbegin();
+
+                if ((interfaces->second).size() == 1) {
+                    std::cout << ToString((*roles)) << " was auto selected for the role." << std::endl
+                              << std::flush;
+                } else {
+                    idx = 1;
+                    std::cout << "Choose role:" << std::endl
+                              << std::flush;
+
+                    while (roles != (interfaces->second).cend()) {
+                        std::cout << " " << unsigned(idx++) << ". : " << ToString((*roles)) << std::endl;
+                        roles++;
+                    }
+
+                    character = toupper(GetUserInput());
+                    uint8_t role = static_cast<uint8_t>(strtol((char*)&character, NULL, 10)) - 1;
+
+                    roles = (interfaces->second).cbegin();
+                    std::advance(roles, role);
+                }
+
+                uRole = *roles;
+            } else {
+                std::cout << "No roles registered on " << interfaces->first << "!!!" << std::endl
+                          << std::flush;
+            }
+        }
+    }
+
+    cec_adapter_role_t SelectRole()
     {
         int character(CEC_LOGICAL_ADDRESS_UNREGISTERED);
 
-        std::cout << "Choose device type:\n"
+        std::cout << "Choose role:\n"
                      "  1 : TV\n"
                      "  2 : Recorder\n"
                      "  3 : Tuner\n"
@@ -257,14 +392,14 @@ private:
 
         uint8_t dev = static_cast<uint8_t>(strtol((char*)&character, NULL, 10));
 
-        return static_cast<role_t>(Convert(_tableDevType, dev, CEC_DEVICE_UNKNOWN));
+        return static_cast<cec_adapter_role_t>(Convert(_tableDevType, dev, CEC_DEVICE_UNKNOWN));
     }
 
     void ShowMenu()
     {
         std::cout << "Enter\n"
-                     "  S : Start Device\n"
                      "  P : Poll all logical addresses\n"
+                     "  V : Scan for CEC versions\n"
                      "  ? : Help\n"
                      "  Q : Quit\n"
                   << std::endl;
@@ -295,10 +430,10 @@ private:
         return strtol((char*)&character, NULL, 16) & 0x0F;
     }
 
+    typedef std::map<string, device_list_t> InterfaceMap;
 private:
     Config _config;
-    WPEFramework::CEC::role_type _role;
-
+    InterfaceMap _interfaces;
 }; // class App
 } // namespace
 
@@ -332,12 +467,14 @@ int main(int argc, const char* argv[])
             tracer.Close();
         }
 
-        std::cout << "Dispose..." << std::endl << std::flush;
+        std::cout << "Dispose...." << std::endl
+                  << std::flush;
 
         WPEFramework::Core::Singleton::Dispose();
     }
 
-    std::cout << "Ok, bye! " << std::endl << std::flush;
+    std::cout << "Ok, bye! " << std::endl
+              << std::flush;
 
     return 0;
 }
