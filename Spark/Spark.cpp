63162dafe3f7f18d7882c45ab96c1da2d664b7f3
HaseenaSainul <41037131+HaseenaSainul@users.noreply.github.com>
2022-05-20 13:55:14 +0530

    Spark: changes to compile and run it again :) (#586)
    * Spark: changes to compile and run it again :)
    
    * Spark: config settings added
    
    Co-authored-by: Pierre Wielders <pierre@wielders.net>

--- Spark/Spark.cpp
+++ Spark/Spark.cpp
@@ -40,7 +40,7 @@ namespace Plugin {
         string message;
 
         ASSERT(_service == nullptr);
-        ASSERT(service != nullptr)
+        ASSERT(service != nullptr);
         ASSERT(_connectionId == 0);
         ASSERT(_spark == nullptr);
         ASSERT(_memory == nullptr);
@@ -88,7 +88,7 @@ namespace Plugin {
         return message;
     }
 
-    /* virtual */ void Spark::Deinitialize(PluginHost::IShell* service)
+    /* virtual */ void Spark::Deinitialize(PluginHost::IShell* service VARIABLE_IS_NOT_USED)
     {
         ASSERT(_service == service);
 
@@ -107,8 +107,8 @@ namespace Plugin {
                 stateControl->Unregister(&_notification);
                 stateControl->Release();
             } else {
-            // On behalf of the crashed process, we will release the notification sink.
-            _   notification.Release();
+                // On behalf of the crashed process, we will release the notification sink.
+                _notification.Release();
             }
 
             if(_memory != nullptr) {
