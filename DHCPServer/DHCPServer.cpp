1bb2c4a5f33d6ac98a06df4eeb5387d5e4929e84
Haseena Sainul <h.sainul@metrological.com>
2022-07-27 05:08:03 -0700

    DHCPServer: remove precondition PLATFORM from the metadata, since it is not valid for this module

--- DHCPServer/DHCPServer.cpp
+++ DHCPServer/DHCPServer.cpp
@@ -29,7 +29,7 @@ namespace Plugin {
             // Version
             1, 0, 0,
             // Preconditions
-            { subsystem::PLATFORM },
+            {},
             // Terminations
             {},
             // Controls
