84ff93df23afb33d403335fec73ef4bd25ff89f4
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2022-12-10 11:33:22 +0100

    Compositor: mesa const up buffer interface

--- Compositor/lib/Mesa/renderer/include/IBuffer.h
+++ Compositor/lib/Mesa/renderer/include/IBuffer.h
@@ -38,14 +38,14 @@ namespace Interfaces {
 
         virtual int Handle() = 0;
 
-        virtual uint32_t Width() = 0; // Width of the allocated buffer in pixels
-        virtual uint32_t Height() = 0; // Height of the allocated buffer in pixels
-        virtual uint32_t Format() = 0; // Layout of a pixel according the fourcc format
-        virtual uint64_t Modifier() = 0; // Pixel arrangement in the buffer, used to optimize for hardware
-        virtual uint8_t Planes() = 0; // The amount of graphic planes available in the buffer.
+        virtual uint32_t Width() const = 0; // Width of the allocated buffer in pixels
+        virtual uint32_t Height() const = 0; // Height of the allocated buffer in pixels
+        virtual uint32_t Format() const = 0; // Layout of a pixel according the fourcc format
+        virtual uint64_t Modifier() const = 0; // Pixel arrangement in the buffer, used to optimize for hardware
+        virtual uint8_t Planes() const = 0; // The amount of graphic planes available in the buffer.
 
-        virtual uint32_t Stride(const uint8_t PlaneId) = 0; // Bytes per row for a plane [(bit-per-pixel/8) * width]
-        virtual uint32_t Offset(const uint8_t PlaneId) = 0; // Offset of the plane from where the pixel data starts in the buffer.
+        virtual uint32_t Stride(const uint8_t PlaneId) const = 0; // Bytes per row for a plane [(bit-per-pixel/8) * width]
+        virtual uint32_t Offset(const uint8_t PlaneId) const = 0; // Offset of the plane from where the pixel data starts in the buffer.
     }; // struct IBuffer
 } // namespace Interfaces
 } // namespace Compositor
