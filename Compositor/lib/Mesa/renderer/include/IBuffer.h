ed6d09d9e9513fc96ee4269e4a4b2d3df2291853
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2022-12-22 10:50:35 +0100

    Compositor: Mesa, update IBuffer

--- Compositor/lib/Mesa/renderer/include/IBuffer.h
+++ Compositor/lib/Mesa/renderer/include/IBuffer.h
@@ -19,49 +19,55 @@
 
 #pragma once
 
-#include <core/core.h>
-
 namespace Compositor {
 namespace Interfaces {
+
     struct EXTERNAL IBuffer {
         /**
-         * @brief   Multi-plane frame buffer interface with hardware optimisation in mind
+         * @brief   frame buffer interface with hardware optimisation in mind
          */
 
-        static constexpr uint8_t PrimaryPlane = 0;
+#if defined(__SIZEOF_POINTER__) && (__SIZEOF_POINTER__ == 8)
+        using buffer_id = uint64_t;
+#else
+        using buffer_id = uint32_t;
+#endif
 
         virtual ~IBuffer() = default;
 
-        virtual uint32_t Lock(uint32_t timeout) const = 0;
-        virtual uint32_t Unlock(uint32_t timeout) const = 0;
+        struct IPlane {
+            virtual ~IPlane() = default;
 
-        virtual WPEFramework::Core::instance_id Accessor(const uint8_t plane) = 0; // Access to the buffer planes.
+            virtual buffer_id Accessor() const = 0; // Access to the actual data.
 
-        virtual int Handle() = 0;
+            virtual uint32_t Stride() const = 0; // Bytes per row for a plane [(bit-per-pixel/8) * width]
+            virtual uint32_t Offset() const = 0; // Offset of the plane from where the pixel data starts in the buffer.
+        };
+
+        struct IIterator {
+            virtual ~IIterator() = default;
+
+            virtual bool IsValid() const = 0;
+            virtual void Reset() = 0;
+            virtual bool Next() = 0;
+
+            virtual IPlane* Plane() = 0;
+        };
+
+        virtual uint32_t AddRef() const = 0;
+        virtual uint32_t Release() const = 0;
+
+        virtual uint32_t Identifier() const = 0;
+
+        virtual IIterator* Planes(const uint32_t timeoutMs) = 0; // Access the planes.
+        virtual uint32_t Release(const bool dirty) = 0; // Called by callee when is done with the planes.
+
+        virtual void Render() = 0; // Mark for scanout.
 
         virtual uint32_t Width() const = 0; // Width of the allocated buffer in pixels
         virtual uint32_t Height() const = 0; // Height of the allocated buffer in pixels
         virtual uint32_t Format() const = 0; // Layout of a pixel according the fourcc format
         virtual uint64_t Modifier() const = 0; // Pixel arrangement in the buffer, used to optimize for hardware
-        virtual uint8_t Planes() const = 0; // The list of id's of graphic planes available in the buffer.
-
-        virtual uint32_t Stride(const uint8_t plane) const = 0; // Bytes per row for a plane [(bit-per-pixel/8) * width]
-        virtual uint32_t Offset(const uint8_t plane) const = 0; // Offset of the plane from where the pixel data starts in the buffer.
-
-        /**
-         * @brief TODO: Maybe we want the planes to abstracted as well, for now go with a 'flattened' API
-         *
-         */
-        // struct IPlane {
-        //     virtual ~IPlane() = default;
-
-        //     virtual WPEFramework::Core::instance_id Accessor() = 0; // Access to the plane.
-
-        //     virtual uint32_t Stride() const = 0; // Bytes per row for a plane [(bit-per-pixel/8) * width]
-        //     virtual uint32_t Offset() const = 0; // Offset of the plane from where the pixel data starts in the buffer.
-        // };
-
-        // virtual IPlane* Plane(const uint8_t plane) = 0;
     }; // struct IBuffer
 } // namespace Interfaces
 } // namespace Compositor
