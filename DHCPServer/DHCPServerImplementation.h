e60a51ce040e402042fc9dd226c73f54f34ba4cd
Mateusz Daniluk <121170681+VeithMetro@users.noreply.github.com>
2024-10-16 10:07:56 +0200

    Using std::copy instead of memcpy doesn't help the compiler
    So it looks like we will have to disable this warning in this case

--- DHCPServer/DHCPServerImplementation.h
+++ DHCPServer/DHCPServerImplementation.h
@@ -167,9 +167,9 @@ namespace Plugin {
 
                 if (_length > maxLength) {
                     _id._allocation = new uint8_t[_length];
-                    std::copy(id, id + _length, _id._allocation);
+                    ::memcpy(_id._allocation, id, _length);
                 } else {
-                    std::copy(id, id + _length, _id._buffer);
+                    ::memcpy(_id._buffer, id, _length);
                 }
             }
             Identifier(const Identifier& copy)
@@ -177,9 +177,9 @@ namespace Plugin {
             {
                 if (_length > maxLength) {
                     _id._allocation = new uint8_t[_length];
-                    std::copy(copy._id._allocation, copy._id._allocation + _length, _id._allocation);
+                    ::memcpy(_id._allocation, copy._id._allocation, _length);
                 } else {
-                    std::copy(copy._id._buffer, copy._id._buffer + _length, _id._buffer);
+                    ::memcpy(_id._buffer, copy._id._buffer, _length);
                 }
             }
             Identifier(const Identifier&& copy)
@@ -189,7 +189,7 @@ namespace Plugin {
                 if (_length > maxLength) {
                     _id._allocation = copy._id._allocation;
                 } else {
-                    std::copy(copy._id._buffer, copy._id._buffer + _length, _id._buffer);
+                    ::memcpy(_id._buffer, copy._id._buffer, _length);
                 }
             }
             ~Identifier()
