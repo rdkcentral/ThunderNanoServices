b8326c262055337493c7d64d9a1e05e4418aeec0
Pierre Wielders <pierre@wielders.net>
2024-11-10 17:54:53 +0100

    Update Compositor.cpp

--- Compositor/lib/Mesa/Compositor.cpp
+++ Compositor/lib/Mesa/Compositor.cpp
@@ -144,7 +144,7 @@ namespace Plugin {
                 const std::string& proxyStubPath,
                 Exchange::IComposition::IDisplay* parentInterface,
                 const Core::ProxyType<RPC::InvokeServer>& engine)
-                : RPC::Communicator(source, proxyStubPath, Core::ProxyType<Core::IIPCServer>(engine), "@Compositor")
+                : RPC::Communicator(source, proxyStubPath, Core::ProxyType<Core::IIPCServer>(engine), _T("@Compositor"))
                 , _parentInterface(parentInterface)
             {
                 if (_parentInterface != nullptr) {
