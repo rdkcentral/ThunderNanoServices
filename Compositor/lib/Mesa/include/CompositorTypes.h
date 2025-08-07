94756bd5ca87b4662f25dc1a149b0a4ca6a5c256
Bram Oosterhuis <dev@bybram.com>
2025-08-07 09:09:34 +0200

    Refactor PixelFormat class: streamline constructors, add IsValid method, and update DefaultModifier

--- Compositor/lib/Mesa/include/CompositorTypes.h
+++ Compositor/lib/Mesa/include/CompositorTypes.h
@@ -19,11 +19,7 @@
 
 #pragma once
 
-#include <core/core.h>
-#include <messaging/messaging.h>
 #include <graphicsbuffer/GraphicsBufferType.h>
-#include <array>
-
 #include <interfaces/IComposition.h>
 #include <interfaces/IGraphicsBuffer.h>
 
@@ -31,106 +27,122 @@ namespace Thunder {
 
 namespace Compositor {
 
-namespace Rectangle {
-    constexpr Exchange::IComposition::Rectangle Default()
-    {
-        return { 0, 0, 0, 0 };
+    namespace Rectangle {
+        constexpr Exchange::IComposition::Rectangle Default()
+        {
+            return { 0, 0, 0, 0 };
+        }
+
+        constexpr bool IsDefault(const Exchange::IComposition::Rectangle& rectangle)
+        {
+            return (rectangle.x == 0) && (rectangle.y == 0) && (rectangle.height == 0) && (rectangle.width == 0);
+        }
     }
 
-    constexpr bool IsDefault(const Exchange::IComposition::Rectangle& rectangle)
-    {
-        return (rectangle.x == 0) && (rectangle.y == 0) && (rectangle.height == 0) && (rectangle.width == 0);
-    }
-}
+    using Identifier = uintptr_t;
+    using Matrix = std::array<float, 9>;
+    using Color = std::array<float, 4>;
 
-using Identifier = uintptr_t;
-using Matrix = std::array<float, 9>;
-using Color = std::array<float, 4>;
-
-constexpr int InvalidFileDescriptor = -1;
-constexpr Identifier InvalidIdentifier = static_cast<Identifier>(~0);
+    constexpr int InvalidFileDescriptor = -1;
+    constexpr Identifier InvalidIdentifier = static_cast<Identifier>(~0);
 
 #ifdef __DEBUG__
-constexpr uint32_t DefaultTimeoutMs = Core::infinite;
+    constexpr uint32_t DefaultTimeoutMs = Core::infinite;
 #else
-constexpr uint32_t DefaultTimeoutMs = 100;
+    constexpr uint32_t DefaultTimeoutMs = 100;
 #endif
 
-/**
- * @brief  A single DRM format, with a set of modifiers attached.
- *
- */
+    constexpr uint64_t DefaultModifier = 0x00FFFFFFFFFFFFFF; // DRM_FORMAT_MOD_INVALID --> Auto select
 
-// using FormatRegister = std::map<uint32_t, std::vector<uint64_t>>;
+    class PixelFormat {
+    private:
+        struct modifier {
+            uint64_t code;
+            bool external;
+        };
 
-class PixelFormat {
-private:
-    static constexpr uint8_t DefaultModifier = 0x00;
+    public:
+        using ModifierType = modifier;
 
-    struct modifier {
-        uint64_t code;
-        bool external;
-    };
+        static const PixelFormat& Default()
+        {
+            static PixelFormat format{};
+            return format;
+        }
 
-public:
-    using ModifierType = modifier;
+        PixelFormat()
+            : _fourcc(0)
+            , _modifiers(1, DefaultModifier)
+        {
+        }
 
-    PixelFormat() = delete;
+        PixelFormat(const uint32_t fourcc, const uint16_t nModifiers, const uint64_t modifiers[])
+            : _fourcc(fourcc)
+            , _modifiers(modifiers, modifiers + nModifiers)
+        {
+        }
+        PixelFormat(const uint32_t fourcc, std::vector<uint64_t>&& modifiers)
+            : _fourcc(fourcc)
+            , _modifiers(std::move(modifiers))
+        {
+        }
+        PixelFormat(const uint32_t fourcc, const std::vector<uint64_t>& modifiers)
+            : _fourcc(fourcc)
+            , _modifiers(modifiers)
+        {
+        }
+        PixelFormat(const uint32_t fourcc)
+            : _fourcc(fourcc)
+            , _modifiers(1, DefaultModifier)
+        {
+        }
+        PixelFormat(PixelFormat&& move)
+            : _fourcc(std::move(move._fourcc))
+            , _modifiers(std::move(move._modifiers))
+        {
+        }
+        PixelFormat(const PixelFormat& copy)
+            : _fourcc(copy._fourcc)
+            , _modifiers(copy._modifiers)
+        {
+        }
+        ~PixelFormat() = default;
 
-    PixelFormat(const uint32_t fourcc, const uint16_t nModifiers, const uint64_t modifiers[])
-        : _fourcc(fourcc)
-        , _modifiers(modifiers, modifiers + nModifiers) {
-    }
-    PixelFormat(const uint32_t fourcc, std::vector<uint64_t>&& modifiers)
-        : _fourcc(fourcc)
-        , _modifiers(std::move(modifiers)) {
-    }
-    PixelFormat(const uint32_t fourcc, const std::vector<uint64_t>& modifiers)
-        : _fourcc(fourcc)
-        , _modifiers(modifiers) {
-    }
-    PixelFormat(const uint32_t fourcc)
-        : _fourcc(fourcc)
-        , _modifiers(1, DefaultModifier)
-    {
-    }
-    PixelFormat(PixelFormat&& move)
-        : _fourcc(std::move(move._fourcc))
-        , _modifiers(std::move(move._modifiers)) {
-    }
-    PixelFormat(const PixelFormat& copy)
-        : _fourcc(copy._fourcc)
-        , _modifiers(copy._modifiers) {
-    }
-    ~PixelFormat() = default;
+        PixelFormat& operator=(PixelFormat&& rhs)
+        {
+            _fourcc = std::move(rhs._fourcc);
+            _modifiers = std::move(rhs._modifiers);
+            return *this;
+        }
+        PixelFormat& operator=(const PixelFormat& rhs)
+        {
+            _fourcc = rhs._fourcc;
+            _modifiers = rhs._modifiers;
+            return *this;
+        }
 
-    PixelFormat& operator=(PixelFormat&& rhs) {
-        _fourcc = std::move(rhs._fourcc);
-        _modifiers = std::move(rhs._modifiers);
-        return *this;
-    }
-    PixelFormat& operator=(const PixelFormat& rhs) {
-        _fourcc = rhs._fourcc;
-        _modifiers = rhs._modifiers;
-        return *this;
-    }
+    public:
+        uint32_t Type() const
+        {
+            return _fourcc;
+        }
 
-public:
-    uint32_t Type() const {
-        return _fourcc;
-    }
+        /*
+         * For details about the format modifiers see `drm_fourcc.h`
+         */
+        const std::vector<uint64_t>& Modifiers() const
+        {
+            return _modifiers;
+        }
 
-    /*
-     * For details about the format modifiers see `drm_fourcc.h` 
-     */
-    const std::vector<uint64_t>& Modifiers() const
-    {
-        return _modifiers;
-    }
+        bool IsValid() const
+        {
+            return (_fourcc != 0) && (_modifiers.size() > 0);
+        }
 
-private:
-     uint32_t _fourcc; // The actual DRM format, from `drm_fourcc.h`
-     std::vector<uint64_t> _modifiers;
-}; // class PixelFormat
+    private:
+        uint32_t _fourcc; // The actual DRM format, from `drm_fourcc.h`
+        std::vector<uint64_t> _modifiers;
+    }; // class PixelFormat
 } // namespace Compositor
 } // namespace Thunder
