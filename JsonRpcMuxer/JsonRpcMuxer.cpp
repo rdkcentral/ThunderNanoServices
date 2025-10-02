e36ac3a8a614338369bb0386643eb963d7459947
Bram Oosterhuis <dev@bybram.com>
2025-10-02 12:17:23 +0200

    JsonRpcMuxer: initial version

---
+++ JsonRpcMuxer/JsonRpcMuxer.cpp
@@ -0,0 +1,178 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2025 Metrological
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "JsonRpcMuxer.h"
+
+namespace Thunder {
+namespace Plugin {
+    namespace {
+        static Metadata<JsonRpcMuxer> metadata(
+            PLUGIN_VERSION_MAJOR, PLUGIN_VERSION_MINOR, PLUGIN_VERSION_PATCH,
+            {}, {}, {});
+    }
+
+    const string JsonRpcMuxer::Initialize(PluginHost::IShell* service)
+    {
+        ASSERT(service != nullptr);
+
+        _service = service;
+        _service->AddRef();
+
+        _dispatch = service->QueryInterfaceByCallsign<PluginHost::IDispatcher>("Controller");
+        ASSERT(_dispatch != nullptr);
+        _dispatch->AddRef();
+
+        return string();
+    }
+
+    void JsonRpcMuxer::Deinitialize(PluginHost::IShell* /*service*/)
+    {
+        _processor.Stop();
+        _processor.Wait(Core::Thread::BLOCKED | Core::Thread::STOPPED, Core::infinite);
+
+        if (_dispatch != nullptr) {
+            _dispatch->Release();
+            _dispatch = nullptr;
+        }
+
+        if (_service != nullptr) {
+            _service->Release();
+            _service = nullptr;
+        }
+    }
+
+    string JsonRpcMuxer::Information() const
+    {
+        return string();
+    }
+
+    bool JsonRpcMuxer::Attach(PluginHost::Channel& channel)
+    {
+        Web::ProtocolsArray protocols = channel.Protocols();
+        return (std::find(protocols.begin(), protocols.end(), string(_T("json"))) != protocols.end());
+    }
+
+    void JsonRpcMuxer::Detach(PluginHost::Channel& channel)
+    {
+    }
+
+    Core::hresult JsonRpcMuxer::Invoke(const uint32_t channelId, const uint32_t id, const string& token,
+        const string& method, const string& parameters, string& response)
+    {
+        uint32_t result(Core::ERROR_NONE);
+
+        if (_dispatch == nullptr) {
+            result = Core::ERROR_UNAVAILABLE;
+            response = _T("Dispatch is offline");
+        } else {
+            Request request(channelId, id, token);
+            Core::OptionalType<Core::JSON::Error> parseError;
+
+            if (!request.FromString(parameters, parseError) || parseError.IsSet()) {
+                result = Core::ERROR_PARSE_FAILURE;
+                response = parseError.IsSet() ? parseError.Value().Message() : _T("Failed to parse message array");
+            } else if (request.Length() == 0) {
+                result = Core::ERROR_BAD_REQUEST;
+                response = _T("Empty message array");
+            } else {
+                result = ~0; // Keep channel open while processing
+                _processor.Add(std::move(request));
+            }
+        }
+
+        return result;
+    }
+
+    uint32_t JsonRpcMuxer::Process(const Request& request)
+    {
+        Core::JSON::ArrayType<Core::JSONRPC::Message>::ConstIterator it = request.Elements();
+        Core::JSON::ArrayType<Response> responseArray;
+
+        while (it.Next()) {
+            const Core::JSONRPC::Message& message = it.Current();
+            string output;
+
+            uint32_t invokeResult = _dispatch->Invoke(
+                request.ChannelId(),
+                message.Id.Value(),
+                request.Token(),
+                message.Designator.Value(),
+                message.Parameters.Value(),
+                output);
+
+            Response& responseMsg = responseArray.Add();
+            responseMsg.Id = message.Id.Value();
+
+            if (invokeResult == Core::ERROR_NONE) {
+                responseMsg.Result = output;
+            } else {
+                responseMsg.Error.SetError(invokeResult);
+                if (!output.empty()) {
+                    responseMsg.Error.Text = output;
+                }
+            }
+        }
+
+        string response;
+        responseArray.ToString(response);
+
+        return SendResponse(request, response);
+    }
+
+    uint32_t JsonRpcMuxer::SendResponse(const Request& request, const string& response)
+    {
+        if (response.empty()) {
+            return Core::ERROR_UNAVAILABLE;
+        }
+
+        Core::ProxyType<Core::JSONRPC::Message> message(PluginHost::IFactories::Instance().JSONRPC());
+        if (!message.IsValid()) {
+            return Core::ERROR_BAD_REQUEST;
+        }
+
+        message->Id = request.ResponseId();
+        message->Result = response;
+
+        return _service->Submit(request.ChannelId(), Core::ProxyType<Core::JSON::IElement>(message));
+    }
+
+    Core::ProxyType<Core::JSON::IElement> JsonRpcMuxer::Inbound(const string& /* identifier */)
+    {
+        return Core::ProxyType<Core::JSON::IElement>(PluginHost::IFactories::Instance().JSONRPC());
+    }
+
+    Core::ProxyType<Core::JSON::IElement> JsonRpcMuxer::Inbound(const uint32_t channelId, const Core::ProxyType<Core::JSON::IElement>& element)
+    {
+        Core::ProxyType<Core::JSONRPC::Message> message(element);
+
+        if (message.IsValid()) {
+            Request request(channelId, message->Id.Value(), "");
+            Core::OptionalType<Core::JSON::Error> parseError;
+
+            if (request.FromString(message->Parameters.Value(), parseError) && request.Length() > 0) {
+                _processor.Add(std::move(request));
+            } else {
+                SendResponse(request, parseError.IsSet() ? parseError.Value().Message() : _T("Invalid batch"));
+            }
+        }
+
+        return Core::ProxyType<Core::JSON::IElement>();
+    }
+}
+}
---
+++ JsonRpcMuxer/JsonRpcMuxer.h
@@ -0,0 +1,195 @@
+#pragma once
+
+#include "Module.h"
+#include <deque>
+
+namespace Thunder {
+namespace Plugin {
+    class JsonRpcMuxer : public PluginHost::IPluginExtended, public PluginHost::JSONRPC, public PluginHost::IWebSocket {
+    private:
+        class Response : public Core::JSON::Container {
+        public:
+            ~Response() override = default;
+
+            Response()
+                : Core::JSON::Container()
+                , Id(~0)
+                , Result(false)
+                , Error()
+            {
+                Add(_T("id"), &Id);
+                Add(_T("result"), &Result);
+                Add(_T("error"), &Error);
+                Clear();
+            }
+
+            Response(const Response& copy)
+                : Core::JSON::Container()
+                , Id(copy.Id)
+                , Result(copy.Result)
+                , Error(copy.Error)
+            {
+                Add(_T("id"), &Id);
+                Add(_T("result"), &Result);
+                Add(_T("error"), &Error);
+            }
+
+            Response(Response&& move) noexcept
+                : Core::JSON::Container()
+                , Id(std::move(move.Id))
+                , Result(std::move(move.Result))
+                , Error(std::move(move.Error))
+            {
+                Add(_T("id"), &Id);
+                Add(_T("result"), &Result);
+                Add(_T("error"), &Error);
+            }
+
+            Core::JSON::DecUInt32 Id;
+            Core::JSON::String Result;
+            Core::JSONRPC::Message::Info Error;
+        };
+
+        class Request : public Core::JSON::ArrayType<Core::JSONRPC::Message> {
+            static uint32_t _sequenceId;
+
+        public:
+            Request() = delete;
+            Request(const uint32_t channelId, const uint32_t responseId, const string& token)
+                : Core::JSON::ArrayType<Core::JSONRPC::Message>()
+                , _tag(Core::InterlockedIncrement(_sequenceId))
+                , _responseId(responseId)
+                , _channelId(channelId)
+                , _token(token)
+            {
+            }
+
+        public:
+            uint32_t BatchId() const { return _tag; }
+            uint32_t ResponseId() const { return _responseId; }
+            uint32_t ChannelId() const { return _channelId; }
+            const string& Token() const { return _token; }
+
+        private:
+            uint32_t _tag;
+            uint32_t _responseId;
+            uint32_t _channelId;
+            string _token;
+        };
+
+        class Processor : public Core::Thread {
+        public:
+            Processor() = delete;
+            Processor(Processor&&) = delete;
+            Processor(const Processor&) = delete;
+            Processor& operator=(Processor&&) = delete;
+            Processor& operator=(const Processor&) = delete;
+
+            Processor(JsonRpcMuxer& parent)
+                : Core::Thread()
+                , _parent(parent)
+                , _adminLock()
+                , _queue()
+            {
+            }
+
+            virtual ~Processor()
+            {
+                Core::Thread::Stop();
+                Core::Thread::Wait(Core::Thread::BLOCKED | Core::Thread::STOPPED, Core::infinite);
+            }
+
+            uint32_t Add(Request&& request)
+            {
+                uint32_t tag = request.BatchId();
+                _adminLock.Lock();
+                _queue.push_back(std::move(request));
+                _adminLock.Unlock();
+                Run();
+                return tag;
+            }
+
+            void Remove(uint32_t tag)
+            {
+                _adminLock.Lock();
+                _queue.erase(
+                    std::remove_if(_queue.begin(), _queue.end(),
+                        [tag](const Request& request) {
+                            return request.BatchId() == tag;
+                        }),
+                    _queue.end());
+                _adminLock.Unlock();
+            }
+
+        private:
+            uint32_t Worker() override
+            {
+                Core::Thread::Block();
+                _adminLock.Lock();
+
+                while (!_queue.empty()) {
+                    Request request(std::move(_queue.front()));
+                    _queue.pop_front();
+                    _adminLock.Unlock();
+                    _parent.Process(request);
+                    _adminLock.Lock();
+                }
+
+                _adminLock.Unlock();
+                return Core::infinite;
+            }
+
+        private:
+            JsonRpcMuxer& _parent;
+            Core::CriticalSection _adminLock;
+            std::deque<Request> _queue;
+        };
+
+    public:
+        JsonRpcMuxer(JsonRpcMuxer&&) = delete;
+        JsonRpcMuxer(const JsonRpcMuxer&) = delete;
+        JsonRpcMuxer& operator=(const JsonRpcMuxer&) = delete;
+
+        JsonRpcMuxer()
+            : _service(nullptr)
+            , _dispatch(nullptr)
+            , _processor(*this)
+        {
+        }
+        ~JsonRpcMuxer() override = default;
+
+        BEGIN_INTERFACE_MAP(JsonRpcMuxer)
+        INTERFACE_ENTRY(PluginHost::IPlugin)
+        INTERFACE_ENTRY(PluginHost::IPluginExtended)
+        INTERFACE_ENTRY(PluginHost::IWebSocket)
+        INTERFACE_ENTRY(PluginHost::IDispatcher)
+        END_INTERFACE_MAP
+
+    public:
+        const string Initialize(PluginHost::IShell* service) override;
+        void Deinitialize(PluginHost::IShell* service) override;
+        string Information() const override;
+
+        bool Attach(PluginHost::Channel& channel) override;
+        void Detach(PluginHost::Channel& channel) override;
+
+        Core::hresult Invoke(
+            const uint32_t channelId, const uint32_t id, const string& token,
+            const string& method, const string& parameters, string& response) override;
+
+        Core::ProxyType<Core::JSON::IElement> Inbound(const string& identifier) override;
+        Core::ProxyType<Core::JSON::IElement> Inbound(const uint32_t ID, const Core::ProxyType<Core::JSON::IElement>& element) override;
+
+    private:
+        uint32_t Process(const Request& request);
+        uint32_t SendResponse(const Request& request, const string& response);
+
+    private:
+        PluginHost::IShell* _service;
+        PluginHost::IDispatcher* _dispatch;
+        Processor _processor;
+    };
+
+    uint32_t JsonRpcMuxer::Request::_sequenceId = 1;
+}
+}
