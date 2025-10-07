f002650147a8974bab875046f38a8ce26a2370f9
Bram Oosterhuis <dev@bybram.com>
2025-10-07 15:45:13 +0200

    CECControl: make LogicAddressClaimer header only

--- CECControl/adapter/device_adapter/src/LogicAddressClaimer.h
+++ CECControl/adapter/device_adapter/src/LogicAddressClaimer.h
@@ -8,6 +8,9 @@
 
 namespace Thunder {
 namespace CEC {
+
+    static constexpr uint16_t CheckInterval = 500;
+    static constexpr uint8_t CheckAttemps = 2;
     class LogicAddressClaimer {
     public:
         struct EXTERNAL ICallback {
@@ -64,7 +67,30 @@ namespace CEC {
             LogicAddressClaimer& _parent;
         };
 
-        uint64_t Check();
+        uint64_t Check()
+        {
+            uint64_t reschedule(0);
+
+            Message msg(0x0F, *(_addresses.begin()));
+
+            ASSERT(_adapter != nullptr);
+
+            if ((_adapter) && (_adapter->Transmit(Convert(msg.Initiator()), msg.Follower(), msg.PayloadLength(), msg.Payload()) == Core::ERROR_NONE)) {
+                _addresses.erase(_addresses.begin());
+                _attemps = (_addresses.empty() != false) ? 0 : CheckAttemps;
+            }
+
+            if (++_attemps < CheckAttemps) {
+                reschedule = Core::Time::Now().Add(CheckInterval).Ticks();
+            } else {
+                _callback.Finished(this);
+                TRACE(Trace::Information, ("%s(%p) claim finished", __FUNCTION__, this));
+            }
+
+            TRACE(Trace::Information, ("%s(%p) next check: %" PRIu64, __FUNCTION__, this, reschedule));
+
+            return reschedule;
+        }
 
         uint32_t Revoke()
         {
