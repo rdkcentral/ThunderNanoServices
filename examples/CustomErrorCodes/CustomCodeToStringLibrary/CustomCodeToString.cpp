3dad823a8659dbe43c26e85ccfeeb359da2c8bf1
MFransen <m.fransen@metrological.com>
2025-11-20 14:30:42 +0100

    [CustomCode] oops r missing

--- examples/CustomErrorCodes/CustomCodeToStringLibrary/CustomCodeToString.cpp
+++ examples/CustomErrorCodes/CustomCodeToStringLibrary/CustomCodeToString.cpp
@@ -21,7 +21,9 @@
 
 #include <core/ICustomErrorCode.h>
 
-const TCHAR* CustomCodeToSting(const int32_t code) {
+sajKSALjsaL
+
+const TCHAR* CustomCodeToString(const int32_t code) {
     const TCHAR* text = nullptr;
     if (code == 100) {
         text = _T("Custom Error code 100");
